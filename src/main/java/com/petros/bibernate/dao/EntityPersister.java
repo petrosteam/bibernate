2bb03ac3f36c85b5dab39402e244f4b6a16c8af3
Ihor Hryban <ihryban@magnite.com>
2023-03-20 16:40:21 +0200

    PTR-12 added update method to EntityPersister

--- src/main/java/com/petros/bibernate/dao/EntityPersister.java
+++ src/main/java/com/petros/bibernate/dao/EntityPersister.java
@@ -17,25 +17,49 @@ import java.util.stream.Collectors;
 
 import static com.petros.bibernate.util.EntityUtil.getColumnName;
 import static com.petros.bibernate.util.EntityUtil.getIdField;
+import static com.petros.bibernate.util.EntityUtil.getIdValue;
 import static com.petros.bibernate.util.EntityUtil.getInsertableColumns;
 import static com.petros.bibernate.util.EntityUtil.getInsertableValues;
 import static com.petros.bibernate.util.EntityUtil.getTableName;
+import static com.petros.bibernate.util.EntityUtil.getUpdatableColumns;
+import static com.petros.bibernate.util.EntityUtil.getUpdatableValues;
 import static java.lang.Boolean.TRUE;
 
+/**
+ * Provides basic functionality for persisting an entity via JDBC.
+ */
 public class EntityPersister {
     private final DataSource dataSource;
-    private static final String FIND_ENTITY_BY_FIELD_NAME = "select * from %s where %s = ?;";
-    private static final String INSERT_INTO_TABLE_VALUES = "insert into %s(%s) values (%s);";
+    private static final String FIND_ENTITY_BY_FIELD_NAME_TEMPLATE = "select * from %s where %s = ?;";
+    private static final String INSERT_INTO_TABLE_VALUES_TEMPLATE = "insert into %s(%s) values (%s);";
+    private static final String UPDATE_BY_ID_TEMPLATE = "update %s set %s where %s = ?;";
 
     public EntityPersister(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
+    /**
+     * Retrieves an entity by its identifier.
+     *
+     * @param entityClass the class of the entity to retrieve
+     * @param idValue     the value of the identifier
+     * @param <T>         the type of the entity
+     * @return the entity with the specified identifier, or null if not found
+     */
     public <T> T findById(Class<T> entityClass, Object idValue) {
         Field idField = getIdField(entityClass);
         return findOne(entityClass, idField, idValue);
     }
 
+    /**
+     * Retrieves a single entity based on a field and its value.
+     *
+     * @param entityClass the class of the entity to retrieve
+     * @param field       the field to search by
+     * @param fieldValue  the value of the field to search by
+     * @param <T>         the type of the entity
+     * @return the entity with the specified field value, or null if not found
+     */
     public <T> T findOne(Class<T> entityClass, Field field, Object fieldValue) {
         List<T> entities = findAll(entityClass, field, fieldValue);
         if (entities.size() == 0) {
@@ -47,13 +71,22 @@ public class EntityPersister {
         return entities.get(0);
     }
 
+    /**
+     * Retrieves all entities based on a field and its value.
+     *
+     * @param entityClass the class of the entity to retrieve
+     * @param field       the field to search by
+     * @param fieldValue  the value of the field to search by
+     * @param <T>         the type of the entity
+     * @return a list of entities with the specified field value
+     */
     public <T> List<T> findAll(Class<T> entityClass, Field field, Object fieldValue) {
         List<T> result = new ArrayList<>();
 
         try (var connection = dataSource.getConnection()) {
             String tableName = getTableName(entityClass);
             String columnName = getColumnName(field);
-            String query = String.format(FIND_ENTITY_BY_FIELD_NAME, tableName, columnName);
+            String query = String.format(FIND_ENTITY_BY_FIELD_NAME_TEMPLATE, tableName, columnName);
 
             try (var statement = connection.prepareStatement(query)) {
                 statement.setObject(1, fieldValue);
@@ -68,15 +101,20 @@ public class EntityPersister {
         return result;
     }
 
+    /**
+     * Inserts a new entity into the database.
+     *
+     * @param entity the entity to insert
+     * @param <T>    the type of the entity
+     * @return the inserted entity with the generated identifier
+     **/
     public <T> T insert(T entity) {
         Objects.requireNonNull(entity);
         try (var connection = dataSource.getConnection()) {
             PreparedStatement insertStatement = prepareInsertStatement(entity, connection);
 
             int rowsAffected = insertStatement.executeUpdate();
-            if (rowsAffected != 1) {
-                throw new BibernateException("Failed to insert entity into the database");
-            }
+            throwExceptionIfRowsAffectedNotOne(rowsAffected, "Failed to insert entity into the database");
             setIdFromGeneratedKeys(entity, insertStatement);
             return entity;
 
@@ -85,16 +123,62 @@ public class EntityPersister {
         }
     }
 
+    /**
+     * Updates an existing entity in the database.
+     *
+     * @param entity the entity to update
+     * @param <T>    the type of the entity
+     * @return the updated entity
+     */
+    public <T> T update(T entity) {
+        Objects.requireNonNull(entity);
+        try (var connection = dataSource.getConnection()) {
+            PreparedStatement updateStatement = prepareUpdateStatement(entity, connection);
+
+            int rowsAffected = updateStatement.executeUpdate();
+            throwExceptionIfRowsAffectedNotOne(rowsAffected, "Failed to update entity in the database");
+            return entity;
+
+        } catch (SQLException | IllegalAccessException e) {
+            throw new BibernateException(e);
+        }
+    }
+
+    private static void throwExceptionIfRowsAffectedNotOne(int rowsAffected, String message) {
+        if (rowsAffected != 1) {
+            throw new BibernateException(message);
+        }
+    }
+
     private static <T> PreparedStatement prepareInsertStatement(T entity, Connection connection) throws SQLException {
         String tableName = getTableName(entity.getClass());
         List<String> columns = getInsertableColumns(entity.getClass());
         List<Object> values = getInsertableValues(entity);
         String insertPlaceHolders = getInsertPlaceholders(columns);
-        String insertQuery = String.format(INSERT_INTO_TABLE_VALUES, tableName, String.join(", ", columns),
-                insertPlaceHolders);
+        String insertQuery = String.format(INSERT_INTO_TABLE_VALUES_TEMPLATE, tableName,
+                String.join(", ", columns), insertPlaceHolders);
 
         PreparedStatement statement = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
         setPreparedStatementValues(values, statement);
+        return statement;
+    }
+
+    private static <T> PreparedStatement prepareUpdateStatement(T entity, Connection connection) throws SQLException,
+            IllegalAccessException {
+        String tableName = getTableName(entity.getClass());
+        Field idField = getIdField(entity.getClass());
+        Object idValue = getIdValue(entity);
+        if (idValue == null) {
+            throw new BibernateException("ID field is null");
+        }
+        List<String> updateColumns = getUpdatableColumns(entity);
+        List<Object> updateValues = getUpdatableValues(entity);
+
+        String updateQuery = String.format(UPDATE_BY_ID_TEMPLATE, tableName,
+                getUpdatePlaceholders(updateColumns), getColumnName(idField));
+        PreparedStatement statement = connection.prepareStatement(updateQuery);
+        setPreparedStatementValues(updateValues, statement);
+        statement.setObject(updateValues.size() + 1, idValue);
         return statement;
     }
 
@@ -115,10 +199,16 @@ public class EntityPersister {
         }
     }
 
-    private static String getInsertPlaceholders(List<?> insertableValues) {
-        return insertableValues.stream()
+    private static String getInsertPlaceholders(List<?> insertableColumns) {
+        return insertableColumns.stream()
                 .map(f -> "?")
                 .collect(Collectors.joining(","));
+    }
+
+    private static String getUpdatePlaceholders(List<?> updatableColumns) {
+        return updatableColumns.stream()
+                .map(column -> column + " = ?")
+                .collect(Collectors.joining(", "));
     }
 
     private static <T> T mapResultSetToEntity(Class<T> entityClass, ResultSet resultSet) {
