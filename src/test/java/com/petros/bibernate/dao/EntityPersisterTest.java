332fdec3e5542de60534dccf7eb8268d829c4798
Ihor Hryban <ihryban@magnite.com>
2023-03-21 10:53:39 +0200

    PTR-11: - implemented the delete method in EntityPersister and covered it with tests. - moved PreparedStatement into try-with-resources to ensure it is automatically closed. - added all SQL types to test data, implemented their handling in Java, and covered with tests.

--- src/test/java/com/petros/bibernate/dao/EntityPersisterTest.java
+++ src/test/java/com/petros/bibernate/dao/EntityPersisterTest.java
@@ -10,9 +10,16 @@ import org.junit.jupiter.api.Test;
 
 import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EntityPersisterTest {
     public static final String DATABASE_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
@@ -47,6 +54,13 @@ public class EntityPersisterTest {
         assertEquals("Play Station", product.getProductName());
         assertEquals("Sony", product.getProducer());
         assertEquals(BigDecimal.valueOf(24900, 2), product.getPrice());
+        assertEquals(LocalDateTime.of(2023, 1, 1, 12, 0, 0), product.getCreatedAt());
+        assertTrue(product.getIsAvailable());
+        assertEquals(100, product.getStockCount());
+        assertEquals(3.0, product.getWeight());
+        assertEquals("Play Station console", product.getDescription());
+        assertEquals(LocalDate.of(2023, 1, 10), product.getSaleDate());
+        assertEquals(LocalTime.of(9, 0, 0), product.getSaleTime());
     }
 
     @Test
@@ -91,12 +105,38 @@ public class EntityPersisterTest {
         product.setProductName("Nintendo Switch");
         product.setProducer("Nintendo");
         product.setPrice(BigDecimal.valueOf(29900, 2));
+        product.setCreatedAt(LocalDateTime.of(2023, 1, 4, 12, 0, 0));
+        product.setIsAvailable(true);
+        product.setStockCount(200);
+        product.setWeight(0.88);
+        product.setDescription("Nintendo Switch console");
+        product.setSaleDate(LocalDate.of(2023, 1, 25));
+        product.setSaleTime(LocalTime.of(9, 0, 0));
 
         Product insertedProduct = entityPersister.insert(product);
 
         assertNotNull(insertedProduct.getId());
         assertEquals(4L, insertedProduct.getId());
         assertEquals(product, insertedProduct);
+
+        // Find the inserted product by ID
+        Product foundProduct = entityPersister.findById(Product.class, insertedProduct.getId());
+
+        // Check if the retrieved product is not null
+        assertNotNull(foundProduct);
+
+        // Compare all the fields of the original product and the retrieved product
+        assertEquals(product.getId(), foundProduct.getId());
+        assertEquals(product.getProductName(), foundProduct.getProductName());
+        assertEquals(product.getProducer(), foundProduct.getProducer());
+        assertEquals(product.getPrice(), foundProduct.getPrice());
+        assertEquals(product.getCreatedAt(), foundProduct.getCreatedAt());
+        assertEquals(product.getIsAvailable(), foundProduct.getIsAvailable());
+        assertEquals(product.getStockCount(), foundProduct.getStockCount());
+        assertEquals(product.getWeight(), foundProduct.getWeight());
+        assertEquals(product.getDescription(), foundProduct.getDescription());
+        assertEquals(product.getSaleDate(), foundProduct.getSaleDate());
+        assertEquals(product.getSaleTime(), foundProduct.getSaleTime());
     }
 
     @Test
@@ -138,7 +178,8 @@ public class EntityPersisterTest {
         product.setProducer("Nintendo");
         product.setPrice(BigDecimal.valueOf(29900, 2));
 
-        assertThrows(BibernateException.class, () -> entityPersister.update(product), "Failed to update entity in the database");
+        assertThrows(BibernateException.class, () -> entityPersister.update(product), "Failed to update entity in the" +
+                " database");
     }
 
     @Test
@@ -158,4 +199,45 @@ public class EntityPersisterTest {
         assertThrows(BibernateException.class, () -> entityPersister.update(product), "ID field is null");
     }
 
+    @Test
+    @DisplayName("Test the delete method with an existing entity")
+    public void testDelete() {
+        Product product = entityPersister.findById(Product.class, 1L);
+
+        assertNotNull(product);
+        entityPersister.delete(product);
+
+        Product deletedProduct = entityPersister.findById(Product.class, 1L);
+        assertNull(deletedProduct);
+    }
+
+    @Test
+    @DisplayName("Test the delete method with a non-existing entity")
+    public void testDeleteNonExistingEntity() {
+        Product product = new Product();
+        product.setId(5L);
+        product.setProductName("Non-existing Product");
+        product.setProducer("Non-existing Producer");
+        product.setPrice(BigDecimal.valueOf(99900, 2));
+
+        assertThrows(BibernateException.class, () -> entityPersister.delete(product), "Failed to delete entity from " +
+                "the database");
+    }
+
+    @Test
+    @DisplayName("Test the delete method with null entity")
+    public void testDeleteWithNullEntity() {
+        assertThrows(NullPointerException.class, () -> entityPersister.delete(null));
+    }
+
+    @Test
+    @DisplayName("Test the delete method with entity with null @Id field")
+    public void testDeleteEntityWithNullId() {
+        Product product = new Product();
+        product.setProductName("Nintendo Switch");
+        product.setProducer("Nintendo");
+        product.setPrice(BigDecimal.valueOf(29900, 2));
+
+        assertThrows(BibernateException.class, () -> entityPersister.delete(product), "ID field is null");
+    }
 }
