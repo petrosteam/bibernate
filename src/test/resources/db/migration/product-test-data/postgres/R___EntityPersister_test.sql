a31729dfa46b2b5cceb1c16cd1fc2f7d26c58833
Ihor Hryban <grybanigor@gmail.com>
2023-03-24 06:51:33 +0200

    PTR-11:Added Testcontainers support for MySQL and PostgreSQL - Implement Testcontainers to run tests with MySQL and PostgreSQL databases - Create separate initialization scripts for test data in PostgreSQL and MySQL+H2 due to syntax differences - Update ci.yaml to ensure Testcontainers compatibility in GitHub Actions

---
+++ src/test/resources/db/migration/product-test-data/postgres/R___EntityPersister_test.sql
@@ -0,0 +1,19 @@
+CREATE TABLE products (
+                          id BIGSERIAL,
+                          name varchar(255) NOT NULL,
+                          producer varchar(255) NOT NULL,
+                          price decimal(10,2) NOT NULL,
+                          created_at TIMESTAMP,
+                          is_available boolean,
+                          stock_count integer,
+                          weight float,
+                          description text,
+                          sale_date date,
+                          sale_time time,
+                          PRIMARY KEY (id)
+);
+
+INSERT INTO products( name, producer, price, created_at, is_available, stock_count, weight, description, sale_date, sale_time)
+VALUES ('Play Station', 'Sony', 249.00, '2023-01-01 12:00:00', true, 100, 3.0, 'Play Station console', '2023-01-10', '09:00:00'),
+       ('XBox', 'Microsoft', 215.00, '2023-01-02 14:00:00', true, 150, 3.5, 'XBox console', '2023-01-15', '10:00:00'),
+       ('Play Station Portable', 'Sony', 150.00, '2023-01-03 16:00:00', true, 75, 1.0, 'Play Station Portable console', '2023-01-20', '11:00:00');
